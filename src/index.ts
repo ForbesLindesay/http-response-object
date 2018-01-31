5ff8b04ece7ea04542d8da0de16027260ee3a041
Forbes Lindesay <forbes@lindesay.co.uk>
2018-01-31 10:24:56 +0000

    Include URL in error and make a status code of `0` be treated as an error

--- src/index.ts
+++ src/index.ts
@@ -10,13 +10,17 @@ class Response<TBody> {
   readonly url: string;
   constructor(statusCode: number, headers: Headers, body: TBody, url: string) {
     if (typeof statusCode !== 'number') {
-      throw new TypeError('statusCode must be a number but was ' + (typeof statusCode));
+      throw new TypeError(
+        'statusCode must be a number but was ' + typeof statusCode,
+      );
     }
     if (headers === null) {
       throw new TypeError('headers cannot be null');
     }
     if (typeof headers !== 'object') {
-      throw new TypeError('headers must be an object but was ' + (typeof headers));
+      throw new TypeError(
+        'headers must be an object but was ' + typeof headers,
+      );
     }
     this.statusCode = statusCode;
     const headersToLowerCase = {};
@@ -27,12 +31,34 @@ class Response<TBody> {
     this.body = body;
     this.url = url;
   }
+  isError(): boolean {
+    return this.statusCode === 0 || this.statusCode >= 400;
+  }
   getBody(encoding: string): string;
   getBody(): TBody;
   getBody(encoding?: string): TBody | string {
+    if (this.statusCode === 0) {
+      var err = new Error(
+        'This request to ' +
+          this.url +
+          ' resulted in a status code of 0. This usually indicates some kind of network error in a browser (e.g. CORS not being set up or the DNS failing to resolve):\n' +
+          this.body.toString(),
+      );
+      (err as any).statusCode = this.statusCode;
+      (err as any).headers = this.headers;
+      (err as any).body = this.body;
+      (err as any).url = this.url;
+      throw err;
+    }
     if (this.statusCode >= 300) {
-      var err = new Error('Server responded with status code '
-                      + this.statusCode + ':\n' + this.body.toString());
+      var err = new Error(
+        'Server responded to ' +
+          this.url +
+          ' with status code ' +
+          this.statusCode +
+          ':\n' +
+          this.body.toString(),
+      );
       (err as any).statusCode = this.statusCode;
       (err as any).headers = this.headers;
       (err as any).body = this.body;
